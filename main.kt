5643fd514ef5562f5f6001970570610490bf624c
MargoAndrianova <157409841+MargoAndrianova@users.noreply.github.com>
2024-01-25 17:50:52 +0200

    Added def and !

--- main.kt
+++ main.kt
@@ -1,1 +1,8 @@
-for i in range(10): print('Hello, World')
+def vivaMechMat():
+       a = print("Viva Mech-mat faculty")
+       return a
+
+for i in range(10):
+	print('Hello, World')
+
+print(vivaMechMat())
